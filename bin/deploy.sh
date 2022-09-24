eef8d5122a55ac12de730bc8ed50a859c327b51d
Ben Prew <ben@throwingbones.com>
2022-09-24 11:40:34 -0700

    Use md5 sum instead of date stamp for deploy
    This better tracks the state of the prod binary. If there are inplace changes I am
    working on locally, I can build locally and identify if those changes are included
    in the prod binary.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 REMOTE_FILE=teamvite-$(date +%F_%H-%M-%S)
-scp teamvite teamvite.com:/var/www/teamvite/$REMOTE_FILE
-ssh teamvite.com ln -sf /var/www/teamvite/$REMOTE_FILE /var/www/teamvite/teamvite
-ssh teamvite.com systemctl --user restart teamvite
+REMOTE_FILE=teamvite-$(md5sum teamvite |head -c8)
+# check if file exists
+if ssh teamvite.com ls "/var/www/teamvite/$REMOTE_FILE"; then
+    ssh teamvite.com touch "/var/www/teamvite/$REMOTE_FILE"
+    echo "file exists, not uploading"
+else
+    scp teamvite "teamvite.com:/var/www/teamvite/$REMOTE_FILE"
+fi
+ssh teamvite.com ln -sf "/var/www/teamvite/$REMOTE_FILE" /var/www/teamvite/teamvite
+echo "restarting service"
+ssh root@teamvite.com systemctl restart teamvite
 # keep the last 3 deploys of teamvite
-ssh teamvite.com 'find /var/www/teamvite/ -type f -name "teamvite-*" -print |sort -r | tail -n +4 |xargs ls'
-ssh teamvite.com 'find /var/www/teamvite/ -type f -name "teamvite-*" -print |sort -r | tail -n +4 |xargs rm'
-ssh teamvite.com service teamvite status
+echo "removing old deploys"
+ssh teamvite.com 'ls -t /var/www/teamvite/teamvite-* | tail -n +4 |xargs -I{} ls {}'
+ssh teamvite.com 'ls -t /var/www/teamvite/teamvite-* | tail -n +4 |xargs -I{} rm {}'
+echo "uptime of service (should be larger than 3s and less than 10s)"
+sleep 3
+ssh teamvite.com systemctl status teamvite | grep -Po ".*; \K(.*)(?= ago)"
+# ssh teamvite.com service teamvite status
